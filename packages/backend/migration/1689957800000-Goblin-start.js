b8fba14884b2aa96051aeff02d987f2b5e157498
Javi A <javi.alvarez@automattic.com>
2023-11-14 13:11:46 +0100

    fixed migration

--- packages/backend/migration/1689957800000-Goblin-start.js
+++ packages/backend/migration/1689957800000-Goblin-start.js
@@ -2,7 +2,7 @@ export class Goblin1689957800000 {
   name = "Goblin1689957800000";
 
   async up(queryRunner) {
-      await queryRunner.query(`ALTER TABLE "note" ADD "reblogtrail" json NOT NULL default {}`);    
+      await queryRunner.query(`ALTER TABLE "note" ADD "reblogtrail" json NULL default '{}'::json`);
   }
 
   async down(queryRunner) {
