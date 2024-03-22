161f8d5e0830ae1de1c94bfd20dd68f1099d764a
Javi A <javi.alvarez@automattic.com>
2024-03-22 17:52:05 +0100

    use different prop to calculate real users

--- packages/backend/src/services/chart/charts/users.ts
+++ packages/backend/src/services/chart/charts/users.ts
@@ -16,8 +16,8 @@ export default class UsersChart extends Chart<typeof schema> {
 
 	protected async tickMajor(): Promise<Partial<KVs<typeof schema>>> {
 		const [localCount, remoteCount] = await Promise.all([
-			Users.countBy({ host: IsNull(), isBot: false }),
-			Users.countBy({ host: Not(IsNull()), isBot: false }),
+			Users.countBy({ host: IsNull(), tumblrUUID: IsNull() }),
+			Users.countBy({ host: Not(IsNull()) }),
 		]);
 
 		return {
