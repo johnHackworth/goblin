d3d3f3392d21f4a12bca23c8b71aaa5eb2e66525
Doug Sheppard <doug@dsheppard.ca>
2024-04-09 02:05:39 -0400

    add hashtag summary header and follow/unfollow buttons

---
+++ packages/client/src/components/MkHashtagSummary.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="hashtag-summary">
+		<div class="name">
+			#{{ props.tag }}
+		</div>
+        <MkFollowHashtagButton :tag="followableHashtag" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {} from "vue";
+import * as misskey from "firefish-js";
+import MkFollowHashtagButton from "@/components/MkFollowHashtagButton.vue";
+
+const props = defineProps<({
+    tag: string;
+})>();
+
+const followableHashtag = props.tag;
+</script>
+
+<style type="text/scss" scoped>
+.hashtag-summary {
+    .name {
+        font-size: large;
+        font-weight: bold;
+        margin-left: 20px;
+    }
+
+    display: flex;
+    justify-content: space-between;
+
+    background-color: white;
+    border-radius: 10px;
+    padding: 1em;
+    margin-bottom: 0.4em;
+}
+</style>
